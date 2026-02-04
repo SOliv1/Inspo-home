522f2db884385dc55dd96fd762994d69b8b83fca
SOliv1 <s.oliver1@icloud.com>
2026-02-04 14:25:16 +0000

    update-weather-fuctions

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,13 @@
-const API_BASE = 'https://api.openweathermap.org/data/2.5';
-const API_KEY = process.env.REACT_APP_WEATHER_KEY;
-
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`)
-  .then(res => res.json())
-  .then(data => console.log(data));
+fetch('/.netlify/functions/getWeather?city=Evesham,GB')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    });
 
 // Evesham, GB (you can change q= to another city)
 export async function fetchCurrentWeather(city = 'Evesham,GB') {
-  const url = `${API_BASE}/weather?q=${encodeURIComponent(
-    city
-  )}&units=metric&appid=${API_KEY}`;
-
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    throw new Error(`Weather request failed: ${res.status}`);
-  }
-
+  const res = await fetch(`/.netlify/functions/getWeather?city=${encodeURIComponent(city)}`);
   const data = await res.json();
+  console.log(data);
   return data;
 }
